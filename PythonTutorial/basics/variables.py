6a1a29ad56763c204db6f0a1c67664e50cd03a7d
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-09-01 14:36:58 +0530

    01-09-24_Part-1

---
+++ PythonTutorial/basics/variables.py
@@ -0,0 +1,56 @@
+'''
+Created on 01-Sep-2024
+
+Variables: Name given to the containers in the memory location which stores i/o data
+
+Syntax:
+
+variable_name = value
+
+'''
+
+# Creation of variables
+
+a = 17.98
+b = 2.4
+c = "vivek"
+d = 4+6j
+e = True
+f = False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+print(a)
+
+"""
+x, y, z = 65, 87.8, "abc"
+print(type(x), type(y), type(z))
+print(x)
+print(type(y))
+print(y)
+print(type(z))
+print(z)
+print(x,y,z)
+"""
+
+
+m=n=o=10 #defining variables with same value in single line
+print(m)
+# print(n) # single line comment
+# print(o)
+
+'''
+# Multi-line comment
+4=s
+"vhj"=r
+4hg=76
+'''
+hjg4=76
+
+
+
