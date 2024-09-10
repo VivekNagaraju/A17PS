333ecf84decbe3c27d33d88438ab7e64bcdc3ad5
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-09-10 08:22:27 +0530

    10-09-24

--- PythonTutorial/control_flow_statements/conditional_statements.py
+++ PythonTutorial/control_flow_statements/conditional_statements.py
@@ -1,14 +1,44 @@
 '''
 Created on 09-Sep-2024
 
-@author: admin
+Conditional statements:
+1. If statement
+2. If-else statement
+3. Series if-else statements
+4. Nested if-else statements
 '''
 
 age=int(input("Please enter your age:"))
 
+'''
 if age >= 18:
     print("Allow inside")  
 else:
     print("Don't allow")
+'''    
+'''
+if age <= 18 and age > 12:
+    print("Teenager")
+     
+elif age >= 60:
+    print("Senior Citizen") 
     
+elif age <=12:
+    print("Child")
+    
+else:
+    print("Adult")
+'''    
+
+if age > 18:
+    if age >= 60:
+        print("Senior Citizen")
+    else:
+        print("Adult")
+        
+else:
+    if age <=12:
+        print("Child")
+    else:
+        print("Teenager")
     
---
+++ PythonTutorial/control_flow_statements/looping_statements.py
@@ -0,0 +1,17 @@
+'''
+Created on 10-Sep-2024
+
+Looping statements: These statements help us to execute any statement/s repeatedly
+
+1. While loop:
+    a. Initialization
+    b. Condition
+    c. Increment/ Decrement
+2. For loop
+'''
+
+a=1
+while a <= 5:
+    a=a+1
+    print("Hello World!")
+    
