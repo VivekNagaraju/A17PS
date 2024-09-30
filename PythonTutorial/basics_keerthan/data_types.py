6f9a3c86d97092023ff4bd126579d151be4f6b0d
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-09-30 08:25:56 +0530

    30-09-24

---
+++ PythonTutorial/basics_keerthan/data_types.py
@@ -0,0 +1,23 @@
+'''
+Numerical: Int, float, complex
+Bool
+Str
+None
+'''
+a=1
+b=3.76
+c="abc"
+d=True
+e=1+8j
+f=None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+a=False
+print(type(a))
+
+
---
+++ PythonTutorial/basics_keerthan/variables.py
@@ -0,0 +1,34 @@
+'''
+Variables:
+- name given to a memory location where data is stored
+- container which stores data
+
+Syntax:
+
+variable_name = value
+'''
+a=1
+print("a=",a)
+
+name="Vivek"
+print("name=",name)
+
+roll_no=1
+print("roll_no=",roll_no)
+
+b, c, d = 10.98, 20.64, 30
+print("b=",b)
+print("c=",c)
+print("d=",d)
+
+e= f= g = 50 # assigning same value to multiple variables
+print("e=",e)
+print("f=",f)
+print("g=",g)
+
+# 10=h # Single-line commenting
+'''
+#Multi-line commenting
+1_name="vivek"
+print=90
+'''
