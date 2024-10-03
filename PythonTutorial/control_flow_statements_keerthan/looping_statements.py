c75d9f1b01c18172930e49fe02fbb6b0607ab444
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-10-03 08:17:25 +0530

    03-10-24

---
+++ PythonTutorial/control_flow_statements_keerthan/looping_statements.py
@@ -0,0 +1,34 @@
+'''
+Looping(Iterations) statements: Statements which execute a group of statements multiple times until a condition is satisfied
+
+Types of loops:
+1. While loop
+2. For loop
+
+1. While loop:
+    - Initialization
+    - Condition
+    - Increment/ Decrement
+    
+2. For loop
+
+Loop control statements:
+1. Break
+2. Continue
+
+'''
+
+'''
+count=5
+
+while count>0:
+    print("Hello world!")
+    # count= count+1
+    count-=1 # += Increment Operator -= Decrement Operator
+'''
+# range(start, stop, step)
+# range(stop)
+# range(start, stop)
+for count in range(5):
+    print("Hello world!")
+    print(count)
